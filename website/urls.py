3bbacfa581d433fe80c4ab779fb18dfa56231f91
Selwan Aboujela <silwan.355@gmail.com>
2025-08-24 13:55:24 +0200

    feat: fix translate page and images from flaticon

--- website/urls.py
+++ website/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
 
     path('', views.home_page,name='home'),
+    path('new', views.news,name='news'),
   
 ]
--- website/views.py
+++ website/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home_page(request):
     return render(request,'index-6-one-page.html')
+
+def news(request):
+    return render(request,'news.html')
